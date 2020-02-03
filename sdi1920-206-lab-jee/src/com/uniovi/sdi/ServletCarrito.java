2bed54966f1d5743f152d2a947e685bbd128621c
Ángel García Menéndez <UO258654@uniovi.es>
2020-02-03 17:31:53 +0100

    SDI – 4 MVC, Modelo Vista Controlador

--- sdi1920-206-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi1920-206-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -24,7 +24,6 @@ public class ServletCarrito extends HttpServlet {
 	 */
 	public ServletCarrito() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -34,7 +33,7 @@ public class ServletCarrito extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito"); // No
+		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito"); 
 
 		// Si no hay carrito, creamos uno
 		if (carrito == null) {
@@ -45,14 +44,10 @@ public class ServletCarrito extends HttpServlet {
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
 		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito) + "<br>");
-		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+		
+		//Retornamos la vista con el parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 
 	private String carritoEnHTML(Map<String, Integer> carrito) {
